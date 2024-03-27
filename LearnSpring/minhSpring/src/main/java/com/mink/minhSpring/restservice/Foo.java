891e57dfe02fe274de0298de356489f2bb3c7e75
LOGIXTEK\minh.tran <minh.tran@logixtek.com>
2024-03-27 18:18:29 +0700

    add some spring practice

---
+++ LearnSpring/minhSpring/src/main/java/com/mink/minhSpring/restservice/Foo.java
@@ -0,0 +1,10 @@
+package com.mink.minhSpring.restservice;
+
+import org.springframework.stereotype.Component;
+
+@Component(value = "")
+public class Foo {
+    public String format() {
+        return "foo";
+    }
+}
--- LearnSpring/minhSpring/src/main/java/com/mink/minhSpring/restservice/GreetingController.java
+++ LearnSpring/minhSpring/src/main/java/com/mink/minhSpring/restservice/GreetingController.java
@@ -2,6 +2,7 @@ package com.mink.minhSpring.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +13,12 @@ public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
+    @Autowired
+    private Foo fooFormatter;
+
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        this.fooFormatter.format();
         return new Greeting(this.counter.incrementAndGet(), String.format(template, name));
     }
 }
